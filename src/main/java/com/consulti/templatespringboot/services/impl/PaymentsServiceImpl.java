55777f9946f639d6b64535570eaf818bff1bf77f
Gibson Gil <114748801+Siriusg2@users.noreply.github.com>
2023-05-26 10:39:40 -0500

    payments service finished

--- src/main/java/com/consulti/templatespringboot/services/impl/PaymentsServiceImpl.java
+++ src/main/java/com/consulti/templatespringboot/services/impl/PaymentsServiceImpl.java
@@ -2,9 +2,9 @@ package com.consulti.templatespringboot.services.impl;
 
 import com.consulti.templatespringboot.models.*;
 import com.consulti.templatespringboot.repositories.PaymentRepository;
+import com.consulti.templatespringboot.repositories.UserRepository;
 import com.consulti.templatespringboot.services.*;
 import com.consulti.templatespringboot.utils.validations.PaymentsValidations;
-
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,56 +19,27 @@ public class PaymentsServiceImpl implements PaymentsService {
   @Autowired
   PaymentsValidations paymentsValidations;
 
+  @Autowired
+  UserRepository userRepository;
+
   @Override
   public List<PaymentsModel> listar() throws Exception {
-    
-      return paymentRepository.findAll();
-  }
-  
-
-  @Override
-  public PaymentsModel save(PaymentsModel request) throws Exception {
-    try {
-      return paymentRepository.save(request);
-    } catch (Exception e) {
-      throw new Exception(
-        "Error al guardar el registro de pago: " + e.getMessage()
-      );
-    }
+    return paymentRepository.findAll();
   }
 
   @Override
-  public PaymentsModel update(PaymentsModel request) throws Exception {
-    Optional<PaymentsModel> optional = paymentRepository.findById(
-      request.getId()
-    );
-    if (optional.isPresent()) {
-      PaymentsModel existingPayment = optional.get();
-      existingPayment.setPeriod(request.getPeriod());
-      existingPayment.setCreatedDate(request.getCreatedDate());
-      existingPayment.setModifiedDate(request.getModifiedDate());
-      existingPayment.setCreatedBy(request.getCreatedBy());
-      existingPayment.setModifiedBy(request.getModifiedBy());
-      existingPayment.setPayment(request.getPayment());
-      // Guardamos los cambios en la base de datos
-      return paymentRepository.save(existingPayment);
-    } else {
-      throw new Exception("No se encontró el registro a actualizar");
-    }
-  }
+  public PaymentsModel save(String idUser) throws Exception {
+    Long userId = Long.parseLong(idUser);
+    Optional<UsersModel> userQuery = userRepository.findById(userId);
 
-  @Override
-  public Boolean delete(PaymentsModel request) throws Exception {
-    try {
-      paymentRepository.deleteById(request.getId());
-      return true;
-    } catch (Exception e) {
-      throw new Exception(
-        "Error deleting payment with id " +
-        request.getId() +
-        ": " +
-        e.getMessage()
-      );
-    }
+    UsersModel user = userQuery.get();
+
+    PaymentsModel payment = new PaymentsModel();
+
+    payment.setCreatedBy(user.getEmail());
+    payment.setPayment(user);
+    PaymentsModel savedPayment = paymentRepository.save(payment);
+
+    return savedPayment;
   }
 }
